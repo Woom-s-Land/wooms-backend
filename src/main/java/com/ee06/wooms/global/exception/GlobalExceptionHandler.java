fb7df31d97f7190094986b94efe1be16aacf11e9
Songdoeon <ehdjs9583@naver.com>
2025-06-30 22:23:51 +0900

    feature : 사진 용량 제한 및 에러 핸들러 추가

--- src/main/java/com/ee06/wooms/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ee06/wooms/global/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.Objects;
 
@@ -18,5 +19,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
         BindingResult result = ex.getBindingResult();
         return ErrorCodeUtils.build(HttpStatus.BAD_REQUEST, Objects.requireNonNull(result.getFieldError()).getDefaultMessage());
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<String> handleMaxSizeException(MaxUploadSizeExceededException ex) {
+        // 413 Payload Too Large
+        return ResponseEntity
+                .status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body("파일 크기가 50MB를 초과했습니다. 더 작은 파일을 업로드해주세요.");
     }
 }

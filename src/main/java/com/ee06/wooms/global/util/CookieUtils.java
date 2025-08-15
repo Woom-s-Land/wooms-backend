ed659affbc64c70d2e7d18b6e8bb9f875bd83af9
Songdoeon <ehdjs9583@naver.com>
2025-08-15 14:47:22 +0900

    config : 쿠키 설정 Properties 추가

--- src/main/java/com/ee06/wooms/global/util/CookieUtils.java
+++ src/main/java/com/ee06/wooms/global/util/CookieUtils.java
@@ -3,13 +3,35 @@ package com.ee06.wooms.global.util;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
 
+@Component
 public class CookieUtils {
+
+    private static boolean secure;
+
+    private static String samesite;
+
+    @Value("${cookie.secure}")
+    public void setSecure(String secure) {
+        this.secure = Boolean.valueOf(secure);
+    }
+
+    @Value("${cookie.samesite}")
+    public void setSamesite(String samesite) {
+        this.samesite = samesite;
+    }
+
     public static String getCookie(HttpServletRequest request, String name) {
         return Optional.ofNullable(request.getCookies())
                 .flatMap(cookies -> Arrays.stream(cookies)
@@ -20,14 +42,20 @@ public class CookieUtils {
     }
 
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        System.out.println(secure);
+        System.out.println(samesite);
+
         ResponseCookie cookie = ResponseCookie.from(name, value)
                 .path("/")
-                .sameSite("None")
-                .httpOnly(true)
-                .secure(true)
+                .sameSite(samesite)
+                .httpOnly(secure)
+                .secure(secure)
                 .maxAge(maxAge)
                 .build();
 
         response.addHeader("Set-Cookie", cookie.toString());
     }
 }
+
+
+

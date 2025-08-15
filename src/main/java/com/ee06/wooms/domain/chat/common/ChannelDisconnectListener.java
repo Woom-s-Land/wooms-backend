285e14f61b6cb78428938d5a739d9be5286ed6eb
Songdoeon <ehdjs9583@naver.com>
2025-08-15 19:56:19 +0900

    feat : disconnect시 개인 큐 위치 전송

--- src/main/java/com/ee06/wooms/domain/chat/common/ChannelDisconnectListener.java
+++ src/main/java/com/ee06/wooms/domain/chat/common/ChannelDisconnectListener.java
@@ -2,6 +2,7 @@ package com.ee06.wooms.domain.chat.common;
 
 import com.ee06.wooms.domain.chat.ChannelRepository;
 import com.ee06.wooms.domain.chat.SessionRepository;
+import com.ee06.wooms.domain.chat.dto.MoveMessage;
 import com.ee06.wooms.domain.chat.entity.Channel;
 import com.ee06.wooms.domain.chat.entity.Woom;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,8 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -25,17 +28,26 @@ public class ChannelDisconnectListener {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
         String sessionId = headerAccessor.getSessionId();
         log.info("Handling disconnect for STOMP session: {}", sessionId);
-
+        var principal = headerAccessor.getUser();
         try {
-            Woom woom = sessionRepository.get(sessionId); // ← 아래에서 null 반환하도록 변경 추천
+            Woom woom = sessionRepository.get(sessionId);
             if (woom != null && woom.getWoomsId() != null) {
                 Channel channel = channelRepository.get(woom.getWoomsId());
                 if (channel != null) {
-                    channel.removeWoom(woom); // Map 기준 제거
-                    // put 불필요
+                    channel.removeWoom(woom);
+                    log.info("Removed woom: {}", woom.getWoomsId());
                     messagingTemplate.convertAndSend("/ws/wooms/disconnect/" + woom.getWoomsId(), woom);
                 }
+                channel.getWooms().forEach(tempWoom -> {
+                    messagingTemplate.convertAndSendToUser(
+                            principal.getName(),
+                            "/queue/init",
+                            MoveMessage.of(tempWoom)
+                    );
+                    log.info("개인 큐 발송 !");
+                });
             }
+
             sessionRepository.remove(sessionId);
         } catch (Exception e) {
             log.error("Error handling disconnect: ", e);
